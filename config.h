5b56bb82440dccb2e8a19ac0d04a1809ffca6b81
Simone Tiberi <tib.sim.98@gmail.com>
2022-04-24 01:40:38 +0200

    Configuration file skeleton added

---
+++ config.h
@@ -0,0 +1,41 @@
+#ifndef __H_MLKM_SHIELD_CONFIG__
+#define __H_MLKM_SHIELD_CONFIG__
+
+/**
+ * comment/uncomment to disable/enable protection for the
+ * memory area where the system call table is defined
+ */
+#define PROTECT_SYS_CALL_TABLE
+
+/**
+ * comment/uncomment to disable/enable protection for the
+ * memory area where the IDT is defined
+ */
+// #define PROTECT_IDT
+
+/**
+ * SAFE_SYMBOLS - null terminated list of additional symbols
+ * to protect (you can identify the symbols by reading what
+ * is returned from /proc/kallsyms)
+ */
+const char *SAFE_SYMBOLS[] = {
+        NULL,
+};
+
+/**
+ * MOMDULE_WHITE_LIST - null terminated list of LKM not to
+ * be analyzed because they are considered 'good' a priori
+ */
+const char *MOMDULE_WHITE_LIST[] = {
+        NULL,
+};
+
+/**
+ * INITIAL_BLACK_LIST - null terminated list of LKM by default
+ * considered malicious (not mounted at all)
+ */
+const char *INITIAL_BLACK_LIST[] = {
+        NULL,
+};
+
+#endif // !__H_MLKM_SHIELD_CONFIG__
