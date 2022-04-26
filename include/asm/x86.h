5b29c8568e7cc30faa8f577ec4bf2499a2664214
Simone Tiberi <tib.sim.98@gmail.com>
2022-04-26 23:50:00 +0200

    Changed from using the hard-coded constant to using a predefined macro for the CR0 bit

--- include/asm/x86.h
+++ include/asm/x86.h
@@ -13,7 +13,7 @@ void force_write_cr0(unsigned long val);
  * These macros allow you to delimit a portion of code that can be accessed
  * in an arbitrary way both on registers and in memory (thanks to the overwriting of cr0)
  */
-#define START_UNPROTECTED_EDITING force_write_cr0(cr0 & ~0x00010000)
+#define START_UNPROTECTED_EDITING force_write_cr0(cr0 & ~X86_CR0_WP)
 #define END_UNPROTECTED_EDITING   force_write_cr0(cr0)
 
 #endif // !_MLKM_SHIELD_X86_H
